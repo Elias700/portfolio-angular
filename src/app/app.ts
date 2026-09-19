0a0e09c3d2128a3fe771423ca72b80beee675779
Elias700 <elias_ribeiro07@outlook.com.br>
2026-09-19 14:38:27 -0300

    feat: criacao componente certificados

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { Header } from "./components/header/header";
 import { Home } from "./components/home/home";
 import { About } from './components/about/about';
 import { Projects } from './projects/projects';
+import { Certificados } from './components/certificados/certificados';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Home, About, Projects],
+  imports: [RouterOutlet, Header, Home, About, Projects, Certificados],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
