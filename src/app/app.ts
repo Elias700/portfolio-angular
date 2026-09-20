500027030fc4f72cbbe123bda5d7dc8b687e08e8
Elias700 <elias_ribeiro07@outlook.com.br>
2026-09-20 09:50:41 -0300

    feat: criacao componente footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,11 +6,12 @@ import { About } from './components/about/about';
 import { Projects } from './projects/projects';
 import { Certificados } from './components/certificados/certificados';
 import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Home, About, Projects, Certificados, Contact],
+  imports: [RouterOutlet, Header, Home, About, Projects, Certificados, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
